aaddc4dfe16ac9f32bf16cfb774451158d112c4f
Taotao Yang <yangtaotao0524@gmail.com>
2025-12-16 15:31:22 +0100

    feat: add example run snippet

--- main.py
+++ main.py
@@ -12,32 +12,31 @@ warnings.filterwarnings(
 
 import multiprocessing
 
-from src.strw_amuse.core import convert, sampler
-from src.strw_amuse.plots import plotter, visualization
+from src.strw_amuse.core import convert
+from src.strw_amuse.plots import visualization
 from src.strw_amuse.sims import mc
 from src.strw_amuse.utils import logger, config
 
 
 if __name__ == "__main__":
+    # make sure to activate the conda env for this run
     # init logger
     logger.setup_logging()
 
     # force mp
     multiprocessing.set_start_method("spawn", force=True)
 
-    # get mc results
+    # get mc results for smaller samples monte carlo simulation ~ 1-2 min real time
     result = mc.monte_carlo_19D(n_samples=10, n_workers=10)
 
-    # local repo
-    dir_path = config.DIR_BASE / "mc" / "mc"
-    file_path = config.DIR_BASE / "mc" / "mc" / "combined_mc.npz"
+    # local dir assignment for results checcking
+    dir_path = config.DIR_BASE / "mc"
+    file_path = config.DIR_BASE / "mc" / "combined_mc.npz"
     outcome_name = "Creative_ionized"
     n_bins = 500
 
-    # combine all mc results into singular file
+    # combine all mc results `.npz` files into singular file
     convert.to_one_npz(dir_path=dir_path, file_path=file_path)
 
     # visualiza mc results
     visualization.visualize(file_path=file_path, outcome_name=outcome_name, n_bins=n_bins)
-    samples_np, samples_sp = sampler.gen_nd_samples()
-    plotter.sampler_nd_coverage_plot(samples_np, samples_sp)
